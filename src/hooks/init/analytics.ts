bf06e45f061b059dc2d258b65c642e1fb86b077b
rpidanny <abhishekmaharjan1993@gmail.com>
2023-05-27 15:20:33 +0200

    feat: add app info

--- src/hooks/init/analytics.ts
+++ src/hooks/init/analytics.ts
@@ -2,8 +2,6 @@ import { Hook } from '@oclif/core';
 import Mixpanel from 'mixpanel';
 import { platform, type, version } from 'os';
 
-import pkg from '../../../package.json';
-
 enum Metrics {
   COMMAND_RUN = 'command_run',
 }
@@ -12,12 +10,17 @@ const hook: Hook<'init'> = async function (opts) {
   const mixpanel = Mixpanel.init('ca278339344787f6b1df246f331e3374');
 
   mixpanel.track(Metrics.COMMAND_RUN, {
-    appVersion: pkg.version,
     command: opts.id,
+    app: {
+      name: this.config.name,
+      version: this.config.version,
+      channel: this.config.channel,
+    },
     os: {
       platform: platform(),
       type: type(),
       version: version(),
+      architecture: this.config.arch,
     },
   });
 };
